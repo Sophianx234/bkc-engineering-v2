a0d9e92b31f4f5768a9167323a2dd9993edc28c9
sophianx234 <dx4336969@gmail.com>
2026-05-16 13:17:00 +0000

    feat: add sitemap

---
+++ next-sitemap.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.bkcengineering.com', 
+  generateRobotsTxt: true, // This automatically generates a robots.txt file alongside the sitemap
+  sitemapSize: 7000,
+}
